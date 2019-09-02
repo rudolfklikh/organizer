f6c623c5fffd01db33e5f056e7cdd1d64c641a5e
RudolfKlikh <rudolfklikh@gmail.com>
2019-09-02 14:18:07 +0200

    Main functionality

--- Organizer/src/app/calendar/calendar.component.ts
+++ Organizer/src/app/calendar/calendar.component.ts
@@ -52,4 +52,8 @@ export class CalendarComponent implements OnInit {
     this.calendar = calendar;
   }
 
+  select(day: moment.Moment) {
+    this.dateService.changeDate(day);
+  }
+
 }
---
+++ Organizer/src/app/shared/tasks.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import * as moment from 'moment';
+
+
+export interface Task {
+  id?: string;
+  title: string;
+  date?: string;
+}
+
+interface CreateResponse {
+  name: string;
+}
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService {
+  static url = 'https://angular-calendar-392d5.firebaseio.com/tasks';
+
+  constructor(private http: HttpClient) { }
+
+  create(task: Task): Observable<Task> {
+    return this.http.post<CreateResponse>(`${TasksService.url}/${task.date}.json`, task)
+    .pipe(map(res => {
+      return {...task, id: res.name};
+    }));
+  }
+
+  load(date: moment.Moment): Observable<Task[]> {
+    return this.http.get<Task[]>(`${TasksService.url}/${date.format('DD-MM-YYYY')}.json`)
+    .pipe(map(tasks => {
+      if (!tasks) {
+        return [];
+      }
+      return Object.keys(tasks).map(key => ({...tasks[key], id: key}));
+    }));
+  }
+
+  remove(task: Task) : Observable<void> {
+    return this.http.delete<void>(`${TasksService.url}/${task.date}/${task.id}.json`);
+  }
+}
